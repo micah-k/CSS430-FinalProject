6b8a0619b59c4459de5411f3e3a828175766bbbe
Micah Kelly <micahk@seank.com>
2015-08-11 15:21:56 -0700

    Fixed debug printouts in read().

--- FileSystem.java
+++ FileSystem.java
@@ -155,7 +155,7 @@
             // If last block isn't entirely full, dont read all of it.
             boolean lastBlock = (fte.inode.length - fte.seekPtr) < Disk.blockSize ||
                                 (fte.inode.length - fte.seekPtr) == 0;
-            SysLib.cout("Last block (" + lastBlock ? "true" : "false" + "). ");
+            SysLib.cout("Last block (" + (lastBlock ? "true" : "false") + "). ");
             readLength = (lastBlock ? (fte.inode.length - fte.seekPtr) : Disk.blockSize);
             SysLib.cout("Read length (" + readLength + "). ");
 
